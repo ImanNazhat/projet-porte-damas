6d43c9a02c6990cbf79ce1bc0c4452d4546b1942
ImanNazhat <emannzhat@gmail.com>
2024-06-12 12:36:16 +0000

    faire le code pour envoyer l'email de validation

--- controllers/AbstractController.php
+++ controllers/AbstractController.php
@@ -1,8 +1,19 @@
 <?php
 
+require_once 'vendor/phpmailer/phpmailer/src/Exception.php';
+require_once 'vendor/phpmailer/phpmailer/src/PHPMailer.php';
+require_once 'vendor/phpmailer/phpmailer/src/SMTP.php';
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\SMTP;
+
 abstract class AbstractController
 {
-     private \Twig\Environment $twig;
+    private \Twig\Environment $twig;
+    
+    private PHPMailer $mail;
+    
     public function __construct()
     {
         $loader = new \Twig\Loader\FilesystemLoader('templates');
@@ -15,12 +26,14 @@ abstract class AbstractController
         $twig->addGlobal('csrf_token', $_SESSION["csrf-token"]);
         
         $this->twig = $twig;
+        
     }
+    
+    
     protected function render(string $template, array $data) : void
     {
         echo $this->twig->render($template, $data); 
     }
-
     protected function redirect(string $route) : void
     {
         header("Location: $route");
@@ -30,5 +43,38 @@ abstract class AbstractController
     {
         echo json_encode($data);
     }
-}
+    
+    protected function sendEmail(string $email,string $name) : void
+    {
+        $mail = new PHPMailer(true);
+        
+         try {          
+                         $mail->SMTPDebug = SMTP::DEBUG_SERVER;    
+                        $mail->isSMTP();
+                        $mail->Host       = 'db.3wa.io';
+                        $mail->Port       = 3306;
+                        $mail->SMTPAuth   = true;
+                        $mail->Username   = 'portededamas5@gmail.com'; 
+                        $mail->Password   = 'efffgtusgfyziijb';
+                        
+                          
+                        $mail->CharSet    = "utf-8";
+                        
+                        $mail->addAddress($email, $name);
+                        $mail->setFrom('portededamas5@gmail.com', 'portededamas');
+                        
+                        $mail->isHTML(true);
+                        
+                        $mail->Subject = 'Confirmation de réservation';
+                        $mail->Body    = "Bonjour $name,<br><br>Votre réservation a été confirmée.<br>><br>Merci de votre réservation.";
+                        $mail->send();
+                    
+                         echo 'Message has been sent';
+                        } catch (Exception) {
+                            echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+                        }
+                                    }
+    }
+
+
 
--- controllers/AuthController.php
+++ controllers/AuthController.php
@@ -1,8 +1,5 @@
 <?php
 
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
-
 
 class AuthController extends AbstractController
                 {
@@ -17,87 +14,38 @@ class AuthController extends AbstractController
                     }
                     
                       public function checkCreateReservation() : void{
-                        
-                    //      if(isset($_POST["name"]) && isset($_POST["email"]) && isset($_POST["telephone"]) && isset($_POST["nombre_personnes"]) && isset($_POST["date"]) && isset($_POST["heure"]) && isset($_POST["message"]))
-                    //         {
-                    //             $name = $_POST["name"];
-                    //             $email = $_POST["email"];
-                    //             $telephone = $_POST["telephone"];
-                    //             $nombrePersonnes = $_POST["nombre_personnes"];
-                    //             $date = $_POST["date"];
-                    //             $heure = $_POST["heure"];
-                    //             $message = $_POST["message"];
+                      
+                     
+                         if(isset($_POST["name"]) && isset($_POST["email"]) && isset($_POST["telephone"]) && isset($_POST["nombre_personnes"]) && isset($_POST["date"]) && isset($_POST["heure"]) && isset($_POST["message"]))
+                            {
+                                $name = $_POST["name"];
+                                $email = $_POST["email"];
+                                $telephone = $_POST["telephone"];
+                                $nombrePersonnes = $_POST["nombre_personnes"];
+                                $date = $_POST["date"];
+                                $heure = $_POST["heure"];
+                                $message = $_POST["message"];
                                 
-                    //             if (empty($name) || empty($email) || empty($telephone) || empty($nombrePersonnes) || empty($date) || empty($heure) || empty($message)) {
-                    //                 $_SESSION["error-message"] = "Tous les champs sont obligatoires. Veuillez les remplir.";
-                    //                 $this->render("main/reserver.html.twig", ['error' => $_SESSION["error-message"]]);
-                    //                 unset($_SESSION["error-message"]);
-                    //                 return;
-                    //             }
+                                if (empty($name) || empty($email) || empty($telephone) || empty($nombrePersonnes) || empty($date) || empty($heure) || empty($message)) {
+                                    $_SESSION["error-message"] = "Tous les champs sont obligatoires. Veuillez les remplir.";
+                                    $this->render("main/reserver.html.twig", ['error' => $_SESSION["error-message"]]);
+                                    unset($_SESSION["error-message"]);
+                                    return;
+                                }
 
-                    //             $reservation = new Reservation($name, $email, $telephone, $nombrePersonnes, $date, $heure, $message);
+                                $reservation = new Reservation($name, $email, $telephone, $nombrePersonnes, $date, $heure, $message);
                                 
-                    //             $reservationManager = new ReservationManager();
+                                $reservationManager = new ReservationManager();
                                 
-                    //             $createdReservation = $reservationManager->createReservation($reservation);
+                                $createdReservation = $reservationManager->createReservation($reservation);
                                 
-                    //             $this->render("main/reserver.html.twig" , []);
-                    //         }
-                    
-                    if (isset($_POST["name"]) && isset($_POST["email"]) && isset($_POST["telephone"]) && isset($_POST["nombre_personnes"]) && isset($_POST["date"]) && isset($_POST["heure"]) && isset($_POST["message"])) {
-                    $name = $_POST["name"];
-                    $email = $_POST["email"];
-                    $telephone = $_POST["telephone"];
-                    $nombrePersonnes = $_POST["nombre_personnes"];
-                    $date = $_POST["date"];
-                    $heure = $_POST["heure"];
-                    $message = $_POST["message"];
-                    
-                    if (empty($name) || empty($email) || empty($telephone) || empty($nombrePersonnes) || empty($date) || empty($heure) || empty($message)) {
-                        $_SESSION["error-message"] = "Tous les champs sont obligatoires. Veuillez les remplir.";
-                        $this->render("main/reserver.html.twig", ['error' => $_SESSION["error-message"]]);
-                        unset($_SESSION["error-message"]);
-                        return;
-                    }
-        
-                    $reservation = new Reservation($name, $email, $telephone, $nombrePersonnes, $date, $heure, $message);
-                    
-                    $reservationManager = new ReservationManager();
-                    $createdReservation = $reservationManager->createReservation($reservation);
-        
-                    // Préparer l'email de confirmation avec PHPMailer
-                    $mail = new PHPMailer(true);
-        
-                   
-                        // Paramètres du serveur
-                   
-                        $mail->isSMTP();
-                        $mail->Host       = 'smtp.gmail.com'; // Configurez votre serveur SMTP
-                        $mail->SMTPAuth   = true;
-                        $mail->Username   = 'portededamas5@gmail.com'; // Votre adresse email SMTP
-                        $mail->Password   = 'efffgtusgfyziijb'; // Votre mot de passe SMTP
-                        $mail->SMTPSecure = 'ssl';
-                        $mail->Port       = 465; // Le port SMTP
-                        
-                         
-                         
-                        // Destinataires
-                        $mail->setFrom('portededamas5@gmail.com', 'portededamas');
-                        $mail->addAddress($email, $name);
-                        $mail->isHTML(true);
-                        // Contenu de l'email
-                       
-                        $mail->Subject = 'Confirmation de réservation';
-                        $mail->Body    = "Bonjour $name,<br><br>Votre réservation a été confirmée.<br>><br>Merci de votre réservation.";
-                        $mail->AltBody = "Bonjour $name,<br><br>Votre réservation a été confirmée.<br>><br>Merci de votre réservation.";
-        
-                        $mail->send();
-                        
-                    }
+                                $this->sendEmail($email,$name);
+                                
+                                $this->render("main/reserver.html.twig" , []);
+                            }
+                 
          }
     
-                    
-                    
                     public function connexion() : void
                     {
                       $this->render("main/connexion.html.twig", []);
